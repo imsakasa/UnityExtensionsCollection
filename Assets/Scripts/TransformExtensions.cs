6c910be3c16710b0aa73ada2d0f3fe22757df6c8
kazusa7 <7218455+kazusa7@users.noreply.github.com>
2020-10-27 23:17:10 +0900

    Add: CollectionExtensionsクラスの追加

--- Assets/Scripts/TransformExtensions.cs
+++ Assets/Scripts/TransformExtensions.cs
@@ -1,89 +1,92 @@
 using UnityEngine;
 
-public static class TransformExtentions
+namespace UnityExtensions
 {
-	#region Position
-	public static void SetPosition(this Transform transform, float x, float y, float z)
+	public static class TransformExtensions
 	{
-		var pos = new Vector3(x, y, z);
-		transform.position = pos;
-	}
+		#region Position
+		public static void SetPosition(this Transform transform, float x, float y, float z)
+		{
+			var pos = new Vector3(x, y, z);
+			transform.position = pos;
+		}
 
-	public static void SetPosX(this Transform transform, float x)
-	{
-		transform.position = new Vector3(x, transform.position.y, transform.position.z);
-	}
+		public static void SetPosX(this Transform transform, float x)
+		{
+			transform.position = new Vector3(x, transform.position.y, transform.position.z);
+		}
 
-	public static void SetPosY(this Transform transform, float y)
-	{
-		transform.position = new Vector3(transform.position.x, y, transform.position.z);
-	}
+		public static void SetPosY(this Transform transform, float y)
+		{
+			transform.position = new Vector3(transform.position.x, y, transform.position.z);
+		}
 
-	public static void SetPosZ(this Transform transform, float z)
-	{
-		transform.position = new Vector3(transform.position.x, transform.position.y, z);
-	}
+		public static void SetPosZ(this Transform transform, float z)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, z);
+		}
 
-	public static void AddPosition(this Transform transform, float x, float y, float z)
-	{
-		transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
-	}
+		public static void AddPosition(this Transform transform, float x, float y, float z)
+		{
+			transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
+		}
 
-	public static void AddPosX(this Transform transform, float x)
-	{
-		transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z);
-	}
+		public static void AddPosX(this Transform transform, float x)
+		{
+			transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z);
+		}
 
-	public static void AddPosY(this Transform transform, float y)
-	{
-		transform.position = new Vector3(transform.position.x, transform.position.y + y, transform.position.z);
-	}
+		public static void AddPosY(this Transform transform, float y)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y + y, transform.position.z);
+		}
 
-	public static void AddPosZ(this Transform transform, float z)
-	{
-		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + z);
-	}
-	#endregion
+		public static void AddPosZ(this Transform transform, float z)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + z);
+		}
+		#endregion
 
-	#region LocalPosition
-	public static void SetLocalPosition(this Transform transform, float x, float y, float z)
-	{
-		var pos = new Vector3(x, y, z);
-		transform.localPosition = pos;
-	}
-	public static void SetLocalPosX(this Transform transform, float x)
-	{
-		transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
-	}
+		#region LocalPosition
+		public static void SetLocalPosition(this Transform transform, float x, float y, float z)
+		{
+			var pos = new Vector3(x, y, z);
+			transform.localPosition = pos;
+		}
+		public static void SetLocalPosX(this Transform transform, float x)
+		{
+			transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+		}
 
-	public static void SetLocalPosY(this Transform transform, float y)
-	{
-		transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
-	}
+		public static void SetLocalPosY(this Transform transform, float y)
+		{
+			transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+		}
 
-	public static void SetLocalPosZ(this Transform transform, float z)
-	{
-		transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
-	}
+		public static void SetLocalPosZ(this Transform transform, float z)
+		{
+			transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
+		}
 
-	public static void AddLocalPosition(this Transform transform, float x, float y, float z)
-	{
-		transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y + y, transform.localPosition.z + z);
-	}
+		public static void AddLocalPosition(this Transform transform, float x, float y, float z)
+		{
+			transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y + y, transform.localPosition.z + z);
+		}
 
-	public static void AddLocalPosX(this Transform transform, float x)
-	{
-		transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z);
-	}
+		public static void AddLocalPosX(this Transform transform, float x)
+		{
+			transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z);
+		}
 
-	public static void AddLocalPosY(this Transform transform, float y)
-	{
-		transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + y, transform.localPosition.z);
-	}
+		public static void AddLocalPosY(this Transform transform, float y)
+		{
+			transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + y, transform.localPosition.z);
+		}
 
-	public static void AddLocalPosZ(this Transform transform, float z)
-	{
-		transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + z);
+		public static void AddLocalPosZ(this Transform transform, float z)
+		{
+			transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + z);
+		}
+		#endregion
 	}
-	#endregion
 }
